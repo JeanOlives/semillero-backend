04f2eb1a6c85a853dfb2490480911dbeb715577a
JuanC <juan.gr97@hotmail.com>
2021-04-23 09:02:27 -0500

    Se agregan archivos de menús

--- scripts/menu-semillero-taller2-es_EC.js
+++ scripts/menu-semillero-taller2-es_EC.js
@@ -5,7 +5,7 @@
 			"MNU_GRPB":"Grupo B",
 			"Grupo A": "Grupo A",
 			"MNU_ESTUDIANTEARC": "Grupo1 - Andres Rueda",
-			"MNU_ESTUDIANTEJCGR":"Grupo1 - Juan Carlos Guzmán",
+			"MNU_ESTUDIANTEJCGR":"Grupo1 - Juan Carlos Guzman",
 			"MNU_ESTUDIANTEVPI": "Grupo1 - Veronica Pineda",
 			"MNU_ESTUDIANTEAFCL":"Grupo1 - Alberto Carrera",
 			"MNU_ESTUDIANTEMAPL":"Grupo1 - Milton Paucar",
