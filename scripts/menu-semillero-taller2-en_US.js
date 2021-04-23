0f89ce260efb630c4ef7e2b617de22dd5a5da531
kary17 <vero_kary1995@hotmail.com>
2021-04-22 19:22:09 -0500

    Se agrega menu grupo A

--- scripts/menu-semillero-taller2-en_US.js
+++ scripts/menu-semillero-taller2-en_US.js
@@ -2,7 +2,10 @@
     "COMMONS": {
 		"MENU" : {
 			"MNU_FASE4":"PHASE 4",
-			"MNU_GRPB":"Grupo B"
+			"MNU_GRPB":"Grupo B",
+			"Grupo A": "Grupo A",
+			"MNU_JCGR_CAP_SEM4": "Estudiante JCGR",
+			"MNU_TALLER_VPI":"Estudiante VPI"
 			
 			
 		}
