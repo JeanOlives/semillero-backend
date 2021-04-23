6f2143e557f27247d0d8e87ee52f1b89f72c2113
miltonAlan <miltonpaucar999@gmail.com>
2021-04-22 19:39:40 -0500

    agregados dos nombres al menu en ingles

--- scripts/menu-semillero-taller2-en_US.js
+++ scripts/menu-semillero-taller2-en_US.js
@@ -5,9 +5,9 @@
 			"MNU_GRPB":"Grupo B",
 			"Grupo A": "Grupo A",
 			"MNU_JCGR_CAP_SEM4": "Estudiante JCGR",
-			"MNU_TALLER_VPI":"Estudiante VPI"
-			
-			
+			"MNU_TALLER_VPI":"Estudiante VPI",
+			"Estudiante MAPL Form": "Estudiante MAPL Form",
+			"MNU_ACA_TALLER_2":"Estudiante AFCL"
 		}
     }
 }
