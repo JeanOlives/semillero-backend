3b3c930dfc08f7880afe9e47882d88e6fbb1a84b
domorales <domorale@espol.edu.ec>
2021-04-22 19:23:40 -0500

    Se añadio etiquetas de los menus del Grupo C

--- scripts/menu-semillero-taller2-en_US.js
+++ scripts/menu-semillero-taller2-en_US.js
@@ -1,10 +1,11 @@
 {
     "COMMONS": {
 		"MENU" : {
-			"MNU_FASE4":"PHASE 4",
-			"MNU_GRPB":"Grupo B"
-			
-			
+			"MNU_FASE4":"FASE 4",
+			"MNU_ESTUDIANTEJCO": "Grupo3 - Jean Olives",
+			"MNU_ESTUDIANTEASH": "Grupo3 - Angelica Serna",
+			"MNU_ESTUDIANTEDSY": "Grupo3 - Dario Sarango",
+			"MNU_ESTUDIANTEDMP": "Grupo3 - Dilan Morales"
 		}
     }
 }
